4421249fd81c10ba2f935108be80958e6e19090d
manuferreira <rmanussa@outlook.com>
2021-10-10 21:25:54 -0300

    Add Entrega-2-topico-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+// função principal
 void main() => runApp(MyApp());
 
+// classe que guarda as configurações do app
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        primaryColor: Colors.purple,
+        brightness: Brightness.dark,
+        primaryColor: Colors.white,
       ),
       home: RandomWords(),
     );
   }
 }
 
-
 class RandomWords extends StatefulWidget {
+  //static const routeName = '/';
+
+  // cria o estado da pagina RandomWords (pagina home)
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
@@ -25,7 +30,152 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
+  final _biggerFont = const TextStyle(fontSize: 16);
+
+
+  // construção da página inicial (RandomWords)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: const Icon(Icons.list, color: Colors.lightBlue), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return Dismissible(
+            key: ValueKey(_suggestions[index]),
+            direction: DismissDirection.endToStart,
+            child: _buildRow(_suggestions[index]),
+            background: Container(
+              color: Colors.red,
+            ),
+            onDismissed: (direction) {
+              setState(() {
+                _deleteItem(_suggestions[index]);
+                _suggestions.removeAt(index);
+              });
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Removed'),
+                backgroundColor: Colors.lightBlue));
+            },
+          );
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: IconButton(
+        icon: alreadySaved ? const Icon(Icons.favorite_rounded) : const Icon(Icons.favorite_border_rounded),
+        color: alreadySaved ? Colors.lightBlue : null,
+        onPressed: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        },
+      ),
+      onTap: () {
+        _editWordPair(context, pair, _suggestions.indexOf(pair));
+      },
+    );
+  }
+
+  // tela de edição do par de palavras
+  void _editWordPair(BuildContext context, WordPair pair, int index) {
+    String? firstWord;
+    String? secondWord;
+    final formKey = GlobalKey<FormState>();
+
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Edit pair')
+            ),
+            body: Container(
+              color: Colors.black12,
+              padding: EdgeInsets.all(20.0),
+              alignment: Alignment.center,
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      initialValue: firstWord,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter the first word',
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'The field is empty, insert a new word';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => firstWord = value,
+                    ),
+                    TextFormField(
+                      initialValue: secondWord,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter the second word',
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'The field is empty, insert a new word';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => secondWord = value,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            formKey.currentState!.save();
+                            setState(() {
+                              var pairUpdated = WordPair(firstWord!, secondWord!);
+                              _suggestions.insert(index, pairUpdated);
+                              Navigator.of(context).pop();
+                            });
+                          }
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text('Save changes')
+                        )
+                      ),
+                ],
+              )
+            ),
+          ));
+        }
+      )
+    );
+  }
+
 
   void _pushSaved() {
     Navigator.of(context).push(
@@ -48,87 +198,18 @@ class _RandomWordsState extends State<RandomWords> {
 
           return Scaffold(
             appBar: AppBar(
-              title: Text('Saved Suggestions'),
+              title: const Text('Saved Suggestions'),
             ),
             body: ListView(children: divided),
           );
         },
       ),
     );
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
   }
 
   void _deleteItem(WordPair pair) {
     _saved.remove(pair);
   }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return Dismissible(
-            key: ValueKey(_suggestions[index]),
-            direction: DismissDirection.endToStart,
-            child: _buildRow(_suggestions[index]),
-            background: Container(
-              color: Colors.pink,
-            ),
-            onDismissed: (direction) {
-              setState(() {
-                _deleteItem(_suggestions[index]);
-                _suggestions.removeAt(index);
-              });
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Removed')));
-            },
-          );
-        }
-    );
-  }
-
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
 
 }
 
